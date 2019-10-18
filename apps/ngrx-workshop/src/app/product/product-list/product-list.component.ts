a87fae42e09e91be04efcad7fb379c2543fa97de
aokrushko <alex.okrushko@gmail.com>
2019-10-18 14:47:09 -0400

    m2: introduce first reducer and selector

--- apps/ngrx-workshop/src/app/product/product-list/product-list.component.ts
+++ apps/ngrx-workshop/src/app/product/product-list/product-list.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 
-import { BasicProduct } from '@ngrx-nx-workshop/api-interfaces';
+import { BasicProduct, Product } from '@ngrx-nx-workshop/api-interfaces';
 import { ProductService } from '../product.service';
 import { RatingService } from '../rating.service';
 import { map, shareReplay } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'ngrx-nx-workshop-home',
@@ -12,17 +13,17 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./product-list.component.scss']
 })
 export class ProductListComponent implements OnInit {
-  products$?: Observable<BasicProduct[]>;
+  products$: Observable<BasicProduct[]> = this.store.select(
+    state => state.product.products
+  );
   customerRatings$?: Observable<Map<string, number>>;
 
   constructor(
-    private readonly productService: ProductService,
+    private readonly store: Store<{ product: { products: Product[] } }>,
     private readonly ratingService: RatingService
   ) {}
 
   ngOnInit() {
-    this.products$ = this.productService.getProducts();
-
     this.customerRatings$ = this.ratingService.getRatings().pipe(
       map(arr => new Map(arr)),
       shareReplay({
