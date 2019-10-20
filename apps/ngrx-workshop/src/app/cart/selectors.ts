b02c42abf39d7af01e37899859a8621be6bd9465
aokrushko <alex.okrushko@gmail.com>
2019-10-20 11:13:48 -0400

    m12: Cart Details view model

--- apps/ngrx-workshop/src/app/cart/selectors.ts
+++ apps/ngrx-workshop/src/app/cart/selectors.ts
@@ -1,6 +1,9 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 import { CART_FEATURE_KEY, CartState } from './reducer';
 
+import * as productSelectors from '../product/selectors';
+import { CartProduct } from '../model/product';
+
 export const cartFeature = createFeatureSelector<CartState>(CART_FEATURE_KEY);
 
 export const getCartItems = createSelector(
@@ -12,4 +15,32 @@ export const getCartItemsCount = createSelector(
   getCartItems,
   cartItems =>
     cartItems ? cartItems.reduce((acc, [, quantity]) => acc + quantity, 0) : 0
+);
+
+export const getCartProducts = createSelector(
+  productSelectors.getProducts,
+  getCartItems,
+  (products, cartItems) => {
+    if (!cartItems || !products) return undefined;
+    return cartItems
+      .map(([id, quantity]): CartProduct | undefined => {
+        const product = products.find(p => p.id === id);
+        if (!product) return undefined;
+        return {
+          ...product,
+          quantity
+        };
+      })
+      .filter((cartProduct): cartProduct is CartProduct => !!cartProduct);
+  }
+);
+
+export const getCartTotal = createSelector(
+  getCartProducts,
+  cartItems =>
+    cartItems &&
+    cartItems.reduce(
+      (acc, product) => acc + product.price * product.quantity,
+      0
+    )
 );
